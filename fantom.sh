d318ecc099d24d1ce6bf5acf95671343ed1a70cb
Atma M Ivancevic <atma.ivancevic@gmail.com>
2018-12-19 16:42:40 -0700

    Create fantom.sh

---
+++ fantom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Can't get the website?!?1?!
+
+
+# FANTOM: unavailable due to security issues
+# The FANTOM web site is temporally unavailable due to security issues.
+# We apologize for the inconvenience.
+# Contact us: fantom-help@riken.jp 
