2c8110f832d0b0ea40a5da4e2ca929974f725a08
Atma M Ivancevic <atma.ivancevic@gmail.com>
2019-01-25 14:21:39 -0700

    Create blueprint.sh

---
+++ blueprint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Fri Jan 25
+
+# download blueprint data 
+# go to deepblue dashboard, experiments grid page: https://deepblue.mpi-inf.mpg.de/dashboard.php#ajax/deepblue_view_grid.php
+# first, download all H3K27ac chipseq bed files
+# In Data types, select: Peaks (35075)
+# In Projects, select: BLUEPRINT Epigenome (2485)
+# In Genome, select: GRCh38 (2485)
+# In Epigenetic Marks, select: H3K27ac (401)
+# Then at the top left, click 'Select all experiments displayed in the Grid'
+# These 401 entries should now appear at the bottom. Click 'Proceed to the download page'
+# Leave Download Options as default
+# At the bottom, click 'Request Download'
+# request id r3488281 (https://deepblue.mpi-inf.mpg.de/request.php?_id=r3488281) 
+# now we wait.....
